13daf6e03ef81692c0b5c255654982f5e4959010
Sakshi19-uttarwar <sakshiuttarwar1234@gmail.com>
2025-10-01 20:13:05 +0530

    half completed

---
+++ main.js
@@ -0,0 +1,76 @@
+/* 1)
+
+let num = prompt("Enter your num:");
+num =Number();
+if (num % 2 == 0){
+    console.log("Even number");
+}
+else{
+    console.log("Odd number");
+}
+*/
+
+/* 2)
+let age = prompt("Enter your age:");
+age = Number();
+if (age <= 18){
+    console.log("You are an adult.");
+}
+else{
+    console.log( "You are a minor.");
+}
+*/
+
+
+/* 3)
+let a = prompt("Enter number a:");
+let b = prompt("Enter number b:");
+num = Number();
+if (a>b){
+    console.log("a is greater than b");
+}
+else {
+    console.log("b is grater than a");
+}
+*/
+
+/* 4)
+let x = prompt("Enter a number:");
+x = Number(x);
+if (x > 0 ){
+    console.log("Number is positive");
+}
+else if (x <= 0){
+    console.log("Number is negative");
+}
+else if (x = 0){
+    console.log("Number is zero");
+}
+else{
+    console.log("Inavlid num!");
+}
+*/
+
+
+/* 5)
+let score = prompt("Enter your score(0-100):");
+score = Number(score);
+if (score >= 90 && score <= 100){
+    console.log("A");
+}
+else if (score >= 80 && score <= 89){
+    console.log("B");
+}
+else if (score >= 70 && score <= 79){
+    console.log("C");
+}
+else if(score >= 60 && score <= 69){
+    console.log("D");
+}
+else if(score < 60 && score >= 0){
+    console.log("F");
+}
+else{
+    console.log("Invalid num!")
+}
+*/
